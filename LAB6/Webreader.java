5aa7237486481857a1766ce27e098259503ab77a
Christian Abdelmassih <chrabd@kth.se>
2016-06-10 00:58:40 +0200

    LAB6: Website render

--- LAB6/Webreader.java
+++ LAB6/Webreader.java
@@ -15,24 +15,29 @@ public class Webreader extends JEditorPane {
   }
 
   public Webreader() {
-    //JFrame frame = new JFrame();
     setEditable(false);
-    //setContentType("text/html");
+    try {
+      setPage("http://www.dn.se");
+    }
+    catch (IOException e) {
+      System.out.println(e);
+      setContentType("text/html");
+      Document doc = kit.createDefaultDocument();
+      setDocument(doc);
+      HTMLEditorKit kit = new HTMLEditorKit();
+      setEditorKit(kit);
+      String htmlString = "<html>\n" + "<body>\n" + "<h1>Welcome!</h1>\n" + "<h2>This is an H2 header</h2>\n" + "<p>This is some sample text</p>\n" + "<p><a href=\"http://devdaily.com/blog/\">devdaily blog</a></p>\n" + "</body>\n";
+      setText(htmlString);
+    }
+
     JScrollPane links = new JScrollPane(this);
-    HTMLEditorKit kit = new HTMLEditorKit();
-    setEditorKit(kit);
-    String htmlString = "<html>\n" + "<body>\n" + "<h1>Welcome!</h1>\n" + "<h2>This is an H2 header</h2>\n" + "<p>This is some sample text</p>\n" + "<p><a href=\"http://devdaily.com/blog/\">devdaily blog</a></p>\n" + "</body>\n";
-
-    Document doc = kit.createDefaultDocument();
-    setDocument(doc);
-    setText(htmlString);
-
     JFrame frame = new JFrame();
-    frame.getContentPane().add(links, BorderLayout.CENTER);
+    frame.getContentPane().add(links);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(new Dimension(300,200));
+    frame.setPreferredSize(new Dimension(800,600));
     frame.setTitle("LAB6");
     frame.setLocationRelativeTo(null);
+    frame.pack();
     frame.setVisible(true);
 
     //JTable table = new JTable(50,2);

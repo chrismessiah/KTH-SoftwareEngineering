7a8918b50fa0af800711bdeab2b9e99f98eb8a9a
Christian Abdelmassih <chrabd@kth.se>
2016-06-10 01:01:56 +0200

    LAB6: New site

--- LAB6/Webreader.java
+++ LAB6/Webreader.java
@@ -15,17 +15,24 @@ public class Webreader extends JEditorPane {
   }
 
   public Webreader() {
+    String webpage;
+    //webpage = "http://www.nada.kth.se/~orjan";
+    webpage = "http://www.nada.kth.se/~ala";
+    //webpage = "http://www.nada.kth.se/~viggo";
+    //webpage = "http://www.nada.kth.se/~vahid";
+    //webpage = "http://www.nada.kth.se/~johanh";
+    //webpage = "http://www.nada.kth.se/~ann";
     setEditable(false);
     try {
-      setPage("http://www.dn.se");
+      setPage(webpage);
     }
     catch (IOException e) {
       System.out.println(e);
       setContentType("text/html");
-      Document doc = kit.createDefaultDocument();
-      setDocument(doc);
       HTMLEditorKit kit = new HTMLEditorKit();
       setEditorKit(kit);
+      Document doc = kit.createDefaultDocument();
+      setDocument(doc);
       String htmlString = "<html>\n" + "<body>\n" + "<h1>Welcome!</h1>\n" + "<h2>This is an H2 header</h2>\n" + "<p>This is some sample text</p>\n" + "<p><a href=\"http://devdaily.com/blog/\">devdaily blog</a></p>\n" + "</body>\n";
       setText(htmlString);
     }
